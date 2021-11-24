efb39498bc21ce6a410e368ab1cd31edd157833f
vinicius <vinicius@houpa.app>
2021-11-23 22:19:07 -0300

    feat: create service players

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,15 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
+import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
 export class PlayersController {
+
+    constructor (private readonly playersService: PlayersService) {}
+    
     @Post()
     async createUpdatePlayer(
         @Body() createPlayerDto: CreatePlayerDto
     ) {
-        const { email } = createPlayerDto;
-        return JSON.stringify(`{
-            "email": "${email}"
-        }`)
+        await this.playersService.createUpdatePlayer(createPlayerDto);
     }
 }
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { CreatePlayerDto } from './dtos/create-player.dto';
+import { IPlayer } from './interfaces/players.interface';
+import * as uuid from 'uuid';
 
 @Injectable()
-export class PlayersService {}
+export class PlayersService {
+
+    private players: IPlayer[] = [];
+
+    private readonly logger = new Logger(PlayersService.name);
+
+    async createUpdatePlayer(CreatePlayerDto: CreatePlayerDto): Promise<void> {
+        this.logger.log(`createPlayerDto: ${CreatePlayerDto}`);
+        await this.create(CreatePlayerDto);
+    }
+
+    private create(CreatePlayerDto: CreatePlayerDto): void {
+        const {name, phoneNumber, email} = CreatePlayerDto;
+
+        const player: IPlayer = {
+            id: uuid.v4(),
+            name,
+            phoneNumber,
+            email,
+            ranking: 'A',
+            positionRanking: 1,
+            urlAvatar: 'https://github.com/viniciusanchieta.png'
+        };
+        this.logger.log(`createPlayerDto: ${JSON.stringify(player)}`);
+        this.players.push(player);
+    }
+}
