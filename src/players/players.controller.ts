47ec85134b3141c3191d15b038caa5dd8cf39ada
vinicius <vinicius@houpa.app>
2021-11-23 21:49:24 -0300

    feat: create DTO of players

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePlayerDto } from './dtos/create-player.dto';
 
 @Controller('api/v1/players')
 export class PlayersController {
     @Post()
-    async createUpdatePlayer() {
-        return JSON.stringify({
-            "name" : "John Doe"
-        });  
+    async createUpdatePlayer(
+        @Body() createPlayerDto: CreatePlayerDto
+    ) {
+        const { email } = createPlayerDto;
+        return JSON.stringify(`{
+            "email": "${email}"
+        }`)
     }
 }
